c1664660021ee6f278a08d314cb08a8d73e5a8bc
1205-sy <61895631+1205-sy@users.noreply.github.com>
2024-01-31 18:03:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def welcome(message: types.Message):
-    await message.reply("Hello! Im Gunther Bot, Please follow my YT channel")
+    await message.reply("Hello! Im Creator Bot, Please follow my YT channel")
 
 @dp.message_handler(commands=['logo'])
 async def logo(message: types.Message):
-    await message.answer_photo('https://avatars.githubusercontent.com/u/62240649?v=4')
+    await message.answer_photo('https://avatars.githubusercontent.com/u/61895631?v=4')
 
 @dp.message_handler()
 async def echo(message: types.Message):
